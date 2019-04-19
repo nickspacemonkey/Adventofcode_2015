04d22f7a21e92d073bda1654bebe1b2da2e8ff78
nickspacemonkey <nickspacemonkey@gmail.com>
2019-04-19 22:38:06 +0100

    Created main funtion that takes 1 argument and renamed variable five to repeat.

--- day4/day4.py
+++ day4/day4.py
@@ -5,21 +5,23 @@ import hashlib
 # result = hashlib.md5(string.encode())
 #
 # print(result.hexdigest())
+def main(puzzle_input):
+    found = False
+    count = 0
+    while found == False:
+        string = puzzle_input + str(count)
+        result = hashlib.md5(string.encode())
+        hex_result = result.hexdigest()
+        repeat = 0
+        for i in range(5):
+            if hex_result[i] == "0":
+                repeat += 1
+        if repeat == 5:
+            print(hex_result)
+            print(count)
+            found = True
+        else:
+            count += 1
 
-puzzle_input = "ckczppom"
-found = False
-count = 0
-while found == False:
-    string = puzzle_input + str(count)
-    result = hashlib.md5(string.encode())
-    hex_result = result.hexdigest()
-    five = 0
-    for i in range(5):
-        if hex_result[i] == "0":
-            five += 1
-    if five == 5:
-        print(hex_result)
-        print(count)
-        found = True
-    else:
-        count += 1
+if __name__ == '__main__':
+    main("ckczppom")
