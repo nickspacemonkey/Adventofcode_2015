8dc913ddb5f9eeaffb56587f2d3d4eeabefeab61
Nicholas Eu <nickspacemonkey@gmail.com>
2019-04-19 22:20:20 +0100

    Solution to day4

--- day4/day4.py
+++ day4/day4.py
@@ -1,7 +1,25 @@
 import hashlib
 
-str = "abcdef609043"
+# string = "abcdef609043"
+#
+# result = hashlib.md5(string.encode())
+#
+# print(result.hexdigest())
 
-result = hashlib.md5(str.encode())
-
-print(result.hexdigest())
+puzzle_input = "ckczppom"
+found = False
+count = 0
+while found == False:
+    string = puzzle_input + str(count)
+    result = hashlib.md5(string.encode())
+    hex_result = result.hexdigest()
+    five = 0
+    for i in range(5):
+        if hex_result[i] == "0":
+            five += 1
+    if five == 5:
+        print(hex_result)
+        print(count)
+        found = True
+    else:
+        count += 1
