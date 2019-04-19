56af4b534ddd76835d0d6a82b926d25fbbd35970
nickspacemonkey <nickspacemonkey@gmail.com>
2019-04-19 23:56:40 +0100

    Removed some commented code and tidied up the while loop.

--- day4/day4ext.py
+++ day4/day4ext.py
@@ -1,21 +1,15 @@
 import hashlib
 
-# string = "abcdef609043"
-#
-# result = hashlib.md5(string.encode())
-#
-# print(result.hexdigest())
 def main(puzzle_input):
-    found = False
     count = 0
-    while found == False:
+    while True:
         string = puzzle_input + str(count)
         result = hashlib.md5(string.encode())
         hex_result = result.hexdigest()
         if hex_result.startswith("000000"):
             print(hex_result)
             print(count)
-            found = True
+            break
         else:
             count += 1
 
